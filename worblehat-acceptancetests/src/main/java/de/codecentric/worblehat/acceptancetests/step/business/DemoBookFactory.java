9fa8d3ffffb5f579cc4a48c2e637066a31127152
vagrant <vagrant@u1604lxde>
2018-06-07 16:15:53 +0200

    Fix acceptance test

--- worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/step/business/DemoBookFactory.java
+++ worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/step/business/DemoBookFactory.java
@@ -7,7 +7,7 @@ public class DemoBookFactory {
     private Book book;
 
     private DemoBookFactory(){
-        this.book = new Book("A book title", "A book author", "1", "1234567890", 2013);
+        this.book = new Book("A book title", "A book author", "1", "1234567890", 2013,"description");
     }
 
     public static DemoBookFactory createDemoBook(){
@@ -34,6 +34,11 @@ public class DemoBookFactory {
         return this;
     }
 
+    public DemoBookFactory withDescription(String description){
+        this.book.setDescription(description);
+        return this;
+    }
+    
     public DemoBookFactory withYearOfPublication(String year){
         this.book.setYearOfPublication(Integer.parseInt(year));
         return this;
