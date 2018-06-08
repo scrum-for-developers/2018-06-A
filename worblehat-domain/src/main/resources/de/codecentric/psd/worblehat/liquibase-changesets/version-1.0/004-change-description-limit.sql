8f6568c1660126466647e6d73183802bbcf85007
vagrant <vagrant@u1604lxde>
2018-06-08 11:09:26 +0200

    [B] Change of description limit now working

--- worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.0/004-change-description-limit.sql
+++ worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.0/004-change-description-limit.sql
@@ -1,5 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset action:change_description_limit
-ALTER TABLE book 
-  ALTER COLUMN description VARCHAR(5000);
+ALTER TABLE book MODIFY description VARCHAR(5000);
